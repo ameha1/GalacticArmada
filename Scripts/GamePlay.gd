098179d55fcb8e8718a0ad4b2c1268e7a11dd3ed
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-06-01 23:48:22 +0300

    gameplay script

--- Scripts/GamePlay.gd
+++ Scripts/GamePlay.gd
@@ -11,23 +11,17 @@ var state = false
 var count = 0
 
 func _ready():
-	
+	#ship flyingenterance to the scene is initialized
 	opener.play("opening_scene")
-	
 
 func _process(delta):
-	
-	
+	#illucinates the perception of speedingup in space
 	if Input.is_action_pressed("accelerate"):
-
 		if stars.speed_scale <= max_starSpeed:
 			stars.speed_scale += 0.001
-			
-			
 	else:
 		if stars.speed_scale >= 0.5:
 			stars.speed_scale -= 0.002
-
 
 func _on_opening_scene_animation_finished(opening_scene):
 		
