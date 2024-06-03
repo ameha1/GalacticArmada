63b1c520f3e2f6e40bd3ab1243ad71bdcb53eb18
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-06-03 23:08:35 +0300

    ...

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -1,23 +1,30 @@
 extends Control
 
+@onready var gameOver = $gameOverPlayer
+
 var playerLife = 8
+var gameOverPullUp = false
 
 func _ready():
 	Signals.on_playerLife_changed.connect(on_playerLife)
-	hide()
 
 func _process(delta):
-	
 	if playerLife <= 0:
-		show()
+		gameOver.play("gameover")
+		return
 	
-func _on_play_again_pressed():
-	
-	get_tree().change_scene_to_file("res://Scenes/MainScene/GamePlay.tscn")
-
+func _on_retry_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MainScene/GamePlay.tscn")	
+#func _on_play_again_pressed():
+	#get_tree().change_scene_to_file("res://Scenes/MainScene/GamePlay.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
 
 func on_playerLife(life):
 	playerLife = life
+
+func _on_about_pressed():
+	get_tree().change_scene_to_file("res://Scenes/CreditsScene/credits.tscn")
+
+
