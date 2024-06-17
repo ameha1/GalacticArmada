4b8499d56f86a183c7b836c7f1585b7ee6ed79b7
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-06-17 21:32:34 +0300

    fix

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var gameOver = $gameOverPlayer
-@onready var screenTransition = $ScreenTransition
+@onready var screenTransition = $SceneTransition
 
 var playerLife = 8
 var gameOverPullUp = false
@@ -10,10 +10,8 @@ func _ready():
 	Signals.on_playerLife_changed.connect(on_playerLife)
 
 func _process(delta):
-	if playerLife <= 0:
-		gameOver.play("gameover")
-		return
-	
+	pass
+
 func _on_retry_pressed():
 	screenTransition.play("fade")
 	await get_tree().create_timer(1).timeout
@@ -26,9 +24,13 @@ func _on_exit_pressed():
 
 func on_playerLife(life):
 	playerLife = life
+	game_Over()
 
 func _on_about_pressed():
 	screenTransition.play("fade")
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://Scenes/CreditsScene/credits.tscn")
 
+func game_Over():
+	if playerLife <= 0:
+		gameOver.play("gameover")
