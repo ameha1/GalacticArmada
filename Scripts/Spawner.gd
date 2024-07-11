1f8de9bdd6b2208e3137db3a8402a5b04269408b
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-07-11 23:48:41 +0300

    spawning mechanic change

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -4,47 +4,69 @@ const MIN_SPAWN_TIME = 1.5
 
 @onready var spawnTimer = $spawnTimer
 
-var preloadedEnemies = [
-	preload("res://Scenes/EnemyScene/enemy.tscn"),
-	preload("res://Scenes/EnemyScene/bouncer_enemy.tscn")
-]
-
-var preloadedElements = [
-	preload("res://Scenes/ShieldPower/shieldPower.tscn"),
-	preload("res://Scenes/RapidFireUp/rapid_fire_up.tscn")
-
-]
-
+var preloadedSteadyEnemy = preload("res://Scenes/EnemyScene/enemy.tscn")
+var preloadedBouncerEnemy =  preload("res://Scenes/EnemyScene/bouncer_enemy.tscn")
 var pMeteor = preload("res://Scenes/MereorScene/meteor.tscn")
 
-var nextSpawnTime = 10
+
+var preloadedShieldPower = preload("res://Scenes/ShieldPower/shieldPower.tscn")
+var preloadedRapidFireUp = preload("res://Scenes/RapidFireUp/rapid_fire_up.tscn")
+
+@onready var HUDscore = $"../CanvasLayer/HUD"
+var score = 0 
+
+@export var nextSpawnTime = 10
 
 func _ready():
 	randomize()
 	spawnTimer.start(nextSpawnTime)
 
+func _process(delta):
+	score = HUDscore.setScore()
+
 func _on_spawn_timer_timeout():
 	var viewRect = get_viewport_rect()
 	var xPos = randi_range(viewRect.position.x,viewRect.end.x)
 	
-	if randf() < 0.1:
+	#if randf() < 0.1:
+		#var meteor = pMeteor.instantiate()
+		#meteor.position = Vector2(xPos,position.y)
+		#get_tree().current_scene.add_child(meteor)
+		#
+		#var elementsPreload = preloadedElements[randi() % preloadedElements.size()]
+		#var element = elementsPreload.instantiate()
+		#element.position = Vector2(xPos,position.y)
+		#get_tree().current_scene.add_child(element)
+		#
+	#else:
+		#var enemyPreload = preloadedEnemies[randi() % preloadedEnemies.size()]
+		#var enemy = enemyPreload.instantiate()
+		#enemy.position = Vector2(xPos,position.y)
+		#
+		#get_tree().current_scene.add_child(enemy)
+	
+	if score % 11 == 0:
 		var meteor = pMeteor.instantiate()
 		meteor.position = Vector2(xPos,position.y)
 		get_tree().current_scene.add_child(meteor)
 		
-		var elementsPreload = preloadedElements[randi() % preloadedElements.size()]
-		var element = elementsPreload.instantiate()
-		element.position = Vector2(xPos,position.y)
-		get_tree().current_scene.add_child(element)
+	if score % 13 == 0:
+		var shield = preloadedShieldPower.instantiate()
+		shield.position = Vector2(xPos,position.y)
+		get_tree().current_scene.add_child(shield)
 		
-	else:
-		var enemyPreload = preloadedEnemies[randi() % preloadedEnemies.size()]
-		var enemy = enemyPreload.instantiate()
-		enemy.position = Vector2(xPos,position.y)
-		
-		get_tree().current_scene.add_child(enemy)
-		
-		
+		var rapidFire = preloadedRapidFireUp.instantiate()
+		rapidFire.position = Vector2(xPos,position.y)
+		get_tree().current_scene.add_child(rapidFire)
+	
+	if score <= 50:
+		var steadyEnemy = preloadedSteadyEnemy.instantiate()
+		steadyEnemy.position = Vector2(xPos,position.y)
+		get_tree().current_scene.add_child(steadyEnemy)
+	elif score <= 150:
+		var bouncerEnemy = preloadedBouncerEnemy.instantiate()
+		bouncerEnemy.position = Vector2(xPos,position.y)
+		get_tree().current_scene.add_child(bouncerEnemy)
 	
 	nextSpawnTime -= 0.1
 	
