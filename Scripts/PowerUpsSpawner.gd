59994dcf289bfe06efca67a8e109f97c5fc8c525
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-07-12 00:07:33 +0300

    spawning config change

--- Scripts/PowerUpsSpawner.gd
+++ Scripts/PowerUpsSpawner.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const MIN_SPAWN_TIME = 1.5
+const MIN_SPAWN_TIME = 200
 
 @onready var spawnTimer = $spawnTimer
 
@@ -10,7 +10,7 @@ var preloadedRapidFireUp = preload("res://Scenes/RapidFireUp/rapid_fire_up.tscn"
 @onready var HUDscore = $"../CanvasLayer/HUD"
 var score = 0 
 
-@export var nextSpawnTime = 10
+@export var nextSpawnTime = 20
 
 func _ready():
 	randomize()
@@ -21,18 +21,19 @@ func _process(delta):
 
 func _on_spawn_timer_timeout():
 	var viewRect = get_viewport_rect()
-	var xPos = randi_range(viewRect.position.x,viewRect.end.x)
+	var xPos1 = randi_range(viewRect.position.x,viewRect.end.x)
+	var xPos2 = randi_range(viewRect.position.x,viewRect.end.x)
 
-	if score % 50 == 0:
-		var shield = preloadedShieldPower.instantiate()
-		shield.position = Vector2(xPos,position.y)
-		get_tree().current_scene.add_child(shield)
 	
-		var rapidFire = preloadedRapidFireUp.instantiate()
-		rapidFire.position = Vector2(xPos,position.y)
-		get_tree().current_scene.add_child(rapidFire)
+	var shield = preloadedShieldPower.instantiate()
+	shield.position = Vector2(xPos1,position.y)
+	get_tree().current_scene.add_child(shield)
 	
-	nextSpawnTime -= 0.1
+	var rapidFire = preloadedRapidFireUp.instantiate()
+	rapidFire.position = Vector2(xPos2,position.y)
+	get_tree().current_scene.add_child(rapidFire)
+	
+	nextSpawnTime += 1
 	
 	if nextSpawnTime < MIN_SPAWN_TIME:
 		nextSpawnTime = MIN_SPAWN_TIME
