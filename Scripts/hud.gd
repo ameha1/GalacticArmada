62ff388c1ac961f3dde2de6167c9ae86863151e8
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-07-15 23:46:14 +0300

    .

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -6,15 +6,19 @@ var plifeIcon = preload("res://Scenes/HUD/life_icon.tscn")
 @onready var Score = $Label
 @onready var spawnerTimeSetter = $"../.."
 
-var score = 0
-var scoresRecord = []
+var score: float = 0.0
+var scoreRecord_res = "res://scoreRecord.txt"
+var scoreFile_W = FileAccess.open(scoreRecord_res,FileAccess.WRITE)
+var scoreFile_R = FileAccess.open(scoreRecord_res,FileAccess.READ)
 
 func _ready():
-
 	clear_lives()
 	Signals.on_playerLife_changed.connect(playerLife_changed)
 	Signals.on_score_increment.connect(_on_score_increment)
-
+	
+func _process(delta):
+	pass
+	
 func clear_lives():
 	for child in lifeContainer.get_children():
 		lifeContainer.remove_child(child)
@@ -25,9 +29,8 @@ func set_lives(lives):
 	for i in range(lives):
 		lifeContainer.add_child(plifeIcon.instantiate())
 	if lives <= 0:
-		scoresRecord.append(score)
-		
-
+		scoreFile_W.store_string(str(score))
+	
 func _on_score_increment(amount):
 	score += amount
 	Score.text = str(score)
@@ -39,6 +42,7 @@ func setScore():
 	return score
 
 func rtrnScoresRecord():
-	return scoresRecord.max()
-
-
+	if FileAccess.file_exists(scoreRecord_res):
+		var bestscore = scoreFile_R.get_as_text()
+		print('bestscore - ')
+		return bestscore 
