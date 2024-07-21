6e0fc4cb6aa213b8a33a5d5fa69cb1d14b4545df
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-07-21 21:01:46 +0100

    relative speed

--- Scripts/GamePlay.gd
+++ Scripts/GamePlay.gd
@@ -4,8 +4,11 @@ var speed = 80
 @onready var stars = $DarkBackGround/stars
 @onready var opener = $openingScene
 @onready var gameScore = $CanvasLayer/HUD
+@onready var powerUpSpeed = $ShieldPower
 
 @export var max_starSpeed = 10
+var max_powerupSpeed = 2
+var min_powerupSpeed = 0.7
 
 var state = false
 var count = 0
@@ -13,16 +16,20 @@ var count = 0
 func _ready():
 	
 	#ship flying enterance to the scene is initialized
+	Signals.powerupSpeed = 0.7
 	opener.play("opening_scene")
 
 func _process(delta):
 	#illucinates the perception of speedingup in space
-	
 	if Input.is_action_pressed("accelerate"):
+		if Signals.powerupSpeed <= max_powerupSpeed:
+			Signals.powerupSpeed += 0.1
 		if stars.speed_scale <= max_starSpeed:
 			stars.speed_scale += 0.002
 	
 	else:
+		if Signals.powerupSpeed > min_powerupSpeed:
+			Signals.powerupSpeed -= 0.1
 		if stars.speed_scale >= 0.5:
 			stars.speed_scale -= 0.003
 
