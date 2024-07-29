5bee93d51609ff8fbc6342f9bc2045f5f334d346
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-07-29 21:14:46 +0100

    spawn missile powerups

--- Scripts/PowerUpsSpawner.gd
+++ Scripts/PowerUpsSpawner.gd
@@ -6,6 +6,8 @@ const MIN_SPAWN_TIME = 200
 
 var preloadedShieldPower = preload("res://Scenes/ShieldPower/shieldPower.tscn")
 var preloadedRapidFireUp = preload("res://Scenes/RapidFireUp/rapid_fire_up.tscn")
+var preloadedMissile = preload("res://Scenes/MissileScene/missile_power.tscn")
+
 
 @onready var HUDscore = $"../CanvasLayer/HUD"
 var score = 0 
@@ -23,8 +25,10 @@ func _on_spawn_timer_timeout():
 	var viewRect = get_viewport_rect()
 	var xPos1 = randi_range(viewRect.position.x,viewRect.end.x)
 	var xPos2 = randi_range(viewRect.position.x,viewRect.end.x)
+	var xPos3 = randi_range(viewRect.position.x,viewRect.end.x)
 	var yPos1 = randi_range(-200,-300)
 	var yPos2 = randi_range(-250,-300)
+	var yPos3 = randi_range(-250,-300)
 
 	
 	var shield = preloadedShieldPower.instantiate()
@@ -36,6 +40,12 @@ func _on_spawn_timer_timeout():
 	var rapidFire = preloadedRapidFireUp.instantiate()
 	rapidFire.position = Vector2(xPos2,yPos2)
 	get_tree().current_scene.add_child(rapidFire)
+	
+	await get_tree().create_timer(5).timeout
+	
+	var missile = preloadedMissile.instantiate()
+	missile.position = Vector2(xPos3,yPos3)
+	get_tree().current_scene.add_child(missile)
 	
 	nextSpawnTime += 5
 	
