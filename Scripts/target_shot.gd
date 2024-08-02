d5928a236e623bd9d06fac751c182cbb1862a3f1
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-08-02 20:29:59 +0100

    ..

--- Scripts/target_shot.gd
+++ Scripts/target_shot.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 @onready var targetShot = $AnimationPlayer
+@onready var target = $TextureRect
 
 func _ready():
 	pass
 
 func targetAcquired():
 	targetShot.play("target_acquired")
+
+func targetLost():
+	target.hide()
+	targetShot.stop()
